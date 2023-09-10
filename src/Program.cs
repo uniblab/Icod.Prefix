07e177b3031aeaf506b4780a9859cf3e3c2c05e9
uniblab <uniblab@hotmail.com>
2023-09-10 19:38:47 -0400

    updating arg logoc

--- src/Program.cs
+++ src/Program.cs
@@ -25,55 +25,74 @@ namespace Icod.Prefix {
 
 		[System.STAThread]
 		public static System.Int32 Main( System.String[] args ) {
-			if ( null == args ) {
-				args = new System.String[ 0 ];
-			}
 			var len = args.Length;
-			if ( ( 1 <= len ) && ( new System.String[] { "--copyright", "-c", "/c" }.Contains( args[ 0 ], System.StringComparer.OrdinalIgnoreCase ) ) ) {
-				PrintCopyright();
-				return 1;
-			} else if ( ( len < 2 ) || ( 6 < len ) ) {
+			if ( 7 < len ) {
 				PrintUsage();
 				return 1;
 			}
-			--len;
-			System.String? inputPathName = null;
-			System.String? outputPathName = null;
-			System.String? prefix = System.String.Empty;
-			System.String @switch;
-			System.Int32 i = -1;
-			do {
-				@switch = args[ ++i ];
-				if ( new System.String[] { "--help", "-h", "/h" }.Contains( inputPathName, System.StringComparer.OrdinalIgnoreCase ) ) {
-					PrintUsage();
-					return 1;
-				} else if ( new System.String[] { "--copyright", "-c", "/c" }.Contains( inputPathName, System.StringComparer.OrdinalIgnoreCase ) ) {
-					PrintCopyright();
-					return 1;
-				} else if ( "--input".Equals( @switch, System.StringComparison.OrdinalIgnoreCase ) ) {
-					inputPathName = args[ ++i ].TrimToNull();
-				} else if ( "--output".Equals( @switch, System.StringComparison.OrdinalIgnoreCase ) ) {
-					outputPathName = args[ ++i ].TrimToNull();
-				} else if ( "--prefix".Equals( @switch, System.StringComparison.OrdinalIgnoreCase ) ) {
-					prefix = args[ ++i ];
-				} else {
-					PrintUsage();
-					return 1;
-				}
-			} while ( i < len );
+
+			var processor = new Icod.Argh.Processor(
+				new Icod.Argh.Definition[] {
+					new Icod.Argh.Definition( "help", new System.String[] { "-h", "--help", "/help" } ),
+					new Icod.Argh.Definition( "copyright", new System.String[] { "-c", "--copyright", "/copyright" } ),
+					new Icod.Argh.Definition( "input", new System.String[] { "-i", "--input", "/input" } ),
+					new Icod.Argh.Definition( "output", new System.String[] { "-o", "--output", "/output" } ),
+					new Icod.Argh.Definition( "prefix", new System.String[] { "-p", "--prefix", "/prefix" } ),
+					new Icod.Argh.Definition( "trim", new System.String[] { "-t", "--trim", "/trim" } ),
+				},
+				System.StringComparer.OrdinalIgnoreCase
+			);
+			processor.Parse( args );
+
+			if ( processor.Contains( "help" ) ) {
+				PrintUsage();
+				return 1;
+			} else if ( processor.Contains( "copyright" ) ) {
+				PrintCopyright();
+				return 1;
+			} else if ( len < 2 ) {
+				PrintUsage();
+				return 1;
+			}
+
+			var trim = processor.Contains( "trim" );
+			System.Func<System.String, System.String?> trimmer;
+			if ( trim ) {
+				trimmer = x => x.TrimToNull();
+			} else {
+				trimmer = x => x;
+			}
+
+			if ( 
+				( !processor.TryGetValue( "prefix", false, out var prefix ) )
+				|| System.String.IsNullOrEmpty( prefix )
+			) {
+				PrintUsage();
+				return 1;
+			}
 
 			System.Func<System.String?, System.Collections.Generic.IEnumerable<System.String>> reader;
-			if ( System.String.IsNullOrEmpty( inputPathName ) ) {
-				reader = a => ReadStdIn();
+			if ( processor.TryGetValue( "input", true, out var inputPathName ) ) {
+				if ( System.String.IsNullOrEmpty( inputPathName ) ) {
+					PrintUsage();
+					return 1;
+				} else {
+					reader = a => ReadFile( a!, trimmer );
+				}
 			} else {
-				reader = a => ReadFile( a! );
+				reader = a => ReadStdIn( trimmer );
 			}
 
 			System.Action<System.String?, System.Collections.Generic.IEnumerable<System.String>> writer;
-			if ( System.String.IsNullOrEmpty( outputPathName ) ) {
-				writer = ( a, b ) => WriteStdOut( b );
+			if ( processor.TryGetValue( "input", true, out var outputPathName ) ) {
+				if ( System.String.IsNullOrEmpty( outputPathName ) ) {
+					PrintUsage();
+					return 1;
+				} else {
+					writer = ( a, b ) => WriteFile( a!, b );
+				}
 			} else {
-				writer = ( a, b ) => WriteFile( a!, b );
+				writer = ( a, b ) => WriteStdOut( b );
 			}
 
 			writer(
@@ -83,13 +102,14 @@ namespace Icod.Prefix {
 				)
 			);
 			return 0;
+
 		}
 
 		private static void PrintUsage() {
 			System.Console.Error.WriteLine( "No, no, no! Use it like this, Einstein:" );
 			System.Console.Error.WriteLine( "Prefix.exe --help" );
 			System.Console.Error.WriteLine( "Prefix.exe --copyright" );
-			System.Console.Error.WriteLine( "Prefix.exe --prefix thePrefix [--input inputFilePathName] [--output outputFilePathName]" );
+			System.Console.Error.WriteLine( "Prefix.exe --prefix thePrefix [--input inputFilePathName] [--output outputFilePathName] [--trim]" );
 			System.Console.Error.WriteLine( "Prefix.exe prefixes each line of input with the specified string." );
 			System.Console.Error.WriteLine( "inputFilePathName and outputFilePathName may be relative or absolute paths." );
 			System.Console.Error.WriteLine( "If inputFilePathName is omitted then input is read from StdIn." );
@@ -100,7 +120,6 @@ namespace Icod.Prefix {
 				"Prefix.exe prefixes each line of input with the specified string.",
 				"Copyright( C ) 2023 Timothy J. Bruce",
 				"",
-				"",
 				"This program is free software: you can redistribute it and / or modify",
 				"it under the terms of the GNU General Public License as published by",
 				"the Free Software Foundation, either version 3 of the License, or",
@@ -108,7 +127,7 @@ namespace Icod.Prefix {
 				"",
 				"This program is distributed in the hope that it will be useful,",
 				"but WITHOUT ANY WARRANTY; without even the implied warranty of",
-				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the",
+				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
 				"GNU General Public License for more details.",
 				"",
 				"You should have received a copy of the GNU General Public License",
@@ -120,26 +139,22 @@ namespace Icod.Prefix {
 		}
 
 		#region io
-		private static System.Collections.Generic.IEnumerable<System.String> ReadStdIn() {
+		private static System.Collections.Generic.IEnumerable<System.String> ReadStdIn( System.Func<System.String, System.String?> trimFunc ) {
 			var line = System.Console.In.ReadLine();
 			while ( null != line ) {
-				line = line.TrimToNull();
+				line = trimFunc( line );
 				if ( null != line ) {
 					yield return line;
 				}
 				line = System.Console.In.ReadLine();
 			}
 		}
-		private static System.Collections.Generic.IEnumerable<System.String> ReadFile( System.String? filePathName ) {
-			filePathName = filePathName?.TrimToNull();
-			if ( System.String.IsNullOrEmpty( filePathName ) ) {
-				throw new System.ArgumentNullException( nameof( filePathName ) );
-			}
+		private static System.Collections.Generic.IEnumerable<System.String> ReadFile( System.String filePathName, System.Func<System.String, System.String?> trimFunc ) {
 			using ( var file = System.IO.File.Open( filePathName, System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read ) ) {
 				using ( var reader = new System.IO.StreamReader( file, System.Text.Encoding.UTF8, true, theBufferSize, true ) ) {
 					var line = reader.ReadLine();
 					while ( null != line ) {
-						line = line.TrimToNull();
+						line = trimFunc( line );
 						if ( null != line ) {
 							yield return line;
 						}
@@ -154,11 +169,7 @@ namespace Icod.Prefix {
 				System.Console.Out.WriteLine( datum );
 			}
 		}
-		private static void WriteFile( System.String? filePathName, System.Collections.Generic.IEnumerable<System.String> data ) {
-			filePathName = filePathName?.TrimToNull();
-			if ( System.String.IsNullOrEmpty( filePathName ) ) {
-				throw new System.ArgumentNullException( nameof( filePathName ) );
-			}
+		private static void WriteFile( System.String filePathName, System.Collections.Generic.IEnumerable<System.String> data ) {
 			using ( var file = System.IO.File.Open( filePathName, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.Write, System.IO.FileShare.None ) ) {
 				_ = file.Seek( 0, System.IO.SeekOrigin.Begin );
 				using ( var writer = new System.IO.StreamWriter( file, System.Text.Encoding.UTF8, theBufferSize, true ) ) {
